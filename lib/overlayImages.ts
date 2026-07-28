ec2ff12dbfa51e9b8a13047b2c8f7224ec71d5c6
Kai <kajus.kunigelis4@gmail.com>
2026-07-28 23:32:09 +0200

    added paths to clothing categories

--- lib/overlayImages.ts
+++ lib/overlayImages.ts
@@ -1,7 +1,20 @@
 import { CategoryId } from './categories';
 
 // Add an entry here each time you draw a new overlay.
-// Categories not listed yet will fall back to the generic box shape.
 export const OVERLAY_IMAGES: Partial<Record<CategoryId, any>> = {
+  'bras': require('../assets/overlays/bras.png'),
+  'short-sleeves': require('../assets/overlays/short-sleeves.png'),
   'long-sleeves': require('../assets/overlays/long-sleeves.png'),
+  'sweaters': require('../assets/overlays/sweaters.png'),
+  'jackets': require('../assets/overlays/jackets.png'),
+  'pants': require('../assets/overlays/pants.png'),
+  'skirts': require('../assets/overlays/skirts.png'),
+  'dresses': require('../assets/overlays/dresses.png'),
+  'shoes': require('../assets/overlays/shoes.png'),
+  'jewelry': require('../assets/overlays/jewelry.png'),
+  'hats': require('../assets/overlays/hats.png'),
+  'belts': require('../assets/overlays/belts.png'),
+  'scarves': require('../assets/overlays/scarves.png'),
+  'tights': require('../assets/overlays/tights.png'),
+  'bags': require('../assets/overlays/bags.png'),
 };
